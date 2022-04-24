f320c80b0c0af081e5ad315cfcaf77de0fa3474c
Wilson Lavrador <wilsonmlavrador@gmail.com>
2022-04-24 04:34:55 -0300

    FIllable das colunas de "Usuarios", correção das colunas de relacionamento.

--- app/Domain/Context/Usuarios/Usuarios.php
+++ app/Domain/Context/Usuarios/Usuarios.php
@@ -2,6 +2,7 @@
 
 namespace App\Domain\Context\Usuarios;
 
+use App\Domain\Context\Empresas\Empresas;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,7 +10,15 @@ class Usuarios extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'nome',
+        'email',
+        'telefone',
+        'data_de_nascimento',
+        'naturalidade'
+    ];
+
     public function empresas(){
-        return $this->belongsToMany(Empresas::class, 'empresas_usuarios', 'id_usuarios', 'id_empresas');
+        return $this->belongsToMany(Empresas::class, 'empresas_usuarios', 'id_usuario', 'id_empresa');
     }
 }
