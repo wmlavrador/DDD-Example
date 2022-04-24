655e700cb0ba4c18987ca28fcbaf7b9a216c6fc1
Wilson Lavrador <wilsonmlavrador@gmail.com>
2022-04-24 20:53:57 -0300

    Repositório do contexto Empresas, com método para criar empresa e retornar todas empresas e seus usuarios relacionados.

--- app/Domain/Context/Empresas/Repositories/EmpresasRepo.php
+++ app/Domain/Context/Empresas/Repositories/EmpresasRepo.php
@@ -2,9 +2,42 @@
 
 namespace App\Domain\Context\Empresas\Repositories;
 
+use App\Domain\Context\Empresas\Empresas;
 use App\Domain\Context\Empresas\Interfaces\EmpresasInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 class EmpresasRepo implements EmpresasInterface
 {
 
+    public function getAll(): Collection
+    {
+        return Empresas::with('usuarios')->get();
+    }
+
+    public function novo(array $data): Empresas
+    {
+        try {
+            $newEmpresa = Empresas::create($data);
+            $newEmpresa->save();
+        } catch (\Exception $exception){
+            throw new \Exception('Não foi possível criar a nova empresa.');
+        }
+
+        return $newEmpresa;
+    }
+
+    public function find(int $id): Empresas
+    {
+        // TODO: Implement find() method.
+    }
+
+    public function atualizar(array $data): Empresas
+    {
+        // TODO: Implement atualizar() method.
+    }
+
+    public function apagar(int $id): bool
+    {
+        // TODO: Implement apagar() method.
+    }
 }
