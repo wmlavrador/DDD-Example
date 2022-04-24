0b286555d7a597861afe6fa922c57bfad3c481d9
Wilson Lavrador <wilsonmlavrador@gmail.com>
2022-04-24 20:13:43 -0300

    Funções base para requests da API

---
+++ app/Http/Requests/BaseApiRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+use function response;
+
+abstract class BaseApiRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    abstract public function rules(): array;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    abstract public function authorize(): bool;
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  Validator  $validator
+     *
+     * @return void
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        $errors = $validator->errors();
+
+        throw new HttpResponseException(response()->json([
+            'errors' => $errors
+        ], Response::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+    protected function getNestedArray(string $alias, array $regras): array
+    {
+        return array_combine(
+            array_map(function ($attribute) use ($alias) {
+                return $alias.$attribute;
+            }, array_keys($regras)),
+            $regras
+        );
+    }
+}
