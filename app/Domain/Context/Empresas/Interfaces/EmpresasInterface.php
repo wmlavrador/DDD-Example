0ddbf495704d70ade023cf877edd82f6daee3325
Wilson Lavrador <wilsonmlavrador@gmail.com>
2022-04-24 20:53:11 -0300

    Criado interface do contexto Empresas

--- app/Domain/Context/Empresas/Interfaces/EmpresasInterface.php
+++ app/Domain/Context/Empresas/Interfaces/EmpresasInterface.php
@@ -2,7 +2,14 @@
 
 namespace App\Domain\Context\Empresas\Interfaces;
 
+use App\Domain\Context\Empresas\Empresas;
+use Illuminate\Database\Eloquent\Collection;
+
 interface EmpresasInterface
 {
-
+    public function getAll(): Collection;
+    public function novo(array $data): Empresas;
+    public function find(int $id): Empresas;
+    public function atualizar(array $data): Empresas;
+    public function apagar(int $id): bool;
 }
